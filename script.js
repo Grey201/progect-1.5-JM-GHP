b18ea79dba1343f374e18bb9e3652eddfc451aa9
Grey <lserg3@yandex.ru>
2020-12-21 21:44:27 +0300

    fourth commit

--- script.js
+++ script.js
@@ -27,37 +27,32 @@ window.addEventListener('resize', () => {
   mobileSlider();
 });
 
-window.onload = function () {
-var button =  document.querySelector('#btn');
-var text = document.querySelector('.main-menu__button_text');
-var expand = document.querySelector('.main-menu__button_expand');
 
-button.onclick = function(){
- 
+  document.addEventListener("DOMContentLoaded", function(event) {
+const button =  document.querySelector('#btn');
+const text = document.querySelector('.main-menu__button_text');
+const expand = document.querySelector('.main-menu__button_expand');
+var elements = document.querySelectorAll('.swiper-slide');
+
+ button.addEventListener('click', function(){
    if (window.innerWidth >= 768 && window.innerWidth < 1120 ){
 
   if (text.innerHTML == 'Показать все') {
     text.innerHTML = 'Скрыть';
      expand.style.transform = 'rotate(180deg)';
-
+for(var i=0; elements.length>0; i++){
+const element=elements[i];
+element.classList.remove('hidden_768');
+}
     
-    ViewSonic.classList.remove('hidden_768');
-    sony.classList.remove('hidden_768'); 
-    apple.classList.remove('hidden_768');
-    samsung.classList.remove('hidden_768');
-    lenovo.classList.remove('hidden_768');
-    hp.classList.remove('hidden_768');
   }
   else {
     text.innerHTML = 'Показать все';
     expand.style.transform = 'rotate(0deg)';
-    ViewSonic.classList.add('hidden_768');
-    sony.classList.add('hidden_768'); 
-    apple.classList.add('hidden_768');
-    samsung.classList.add('hidden_768');
-    lenovo.classList.add('hidden_768');
-    hp.classList.add('hidden_768');
-    
+    for(var i=6; elements.length>0; i++){
+      const element=elements[i];
+    element.classList.add('hidden_768');
+    }
   return false;
 }
    }
@@ -66,23 +61,22 @@ button.onclick = function(){
       if( text.innerHTML == 'Показать все') {
       text.innerHTML = 'Скрыть';
        expand.style.transform = 'rotate(180deg)';
-      apple.classList.remove('hidden_1120');
-      samsung.classList.remove('hidden_1120');
-      lenovo.classList.remove('hidden_1120');
-      
+      for(var i=0; elements.length>0; i++){
+        const element=elements[i];
+        element.classList.remove('hidden_1120');
+      }
     }
     else {
       text.innerHTML = 'Показать все';
       expand.style.transform = 'rotate(0deg)';
-      
-      apple.classList.add('hidden_1120');
-      samsung.classList.add('hidden_1120');
-      lenovo.classList.add('hidden_1120');
-      
+      for(var i=8; elements.length>0; i++){
+        const element=elements[i];
+        element.classList.add('hidden_1120');
+      }
       
     return false;
    }
   }
   
-};
-};
+});
+});
